d751aa8e391c13f3e9c4e8062adeccb38efc12a2
Craig Mayhew <craig@mayhew.io>
2015-07-23 22:12:56 +0100

    Create session3-part4.js

---
+++ session3-part4.js
@@ -0,0 +1,5 @@
+  $(document).ready(function() {
+    /*do something cool, but only when the page is fully loaded*/
+    $(“body”).css(“background-color”,”#3399FF”);
+    $(“body”).append(“<div>I’m a dynamically created element!</div>”)
+  });
