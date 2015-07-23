f5764ccaab49bba816b0f8f2e6e4626ba06e161a
Craig Mayhew <craig@mayhew.io>
2015-07-23 22:16:05 +0100

    Create session3-part7.js

---
+++ session3-part7.js
@@ -0,0 +1,11 @@
+
+
+  /*do something cool, but only when the page is fully loaded*/
+  $(document).ready(function() {
+    /*activate the jquery plugin called bxSlider*/
+    $('.bxslider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      slideMargin: 10
+    });
+  });
